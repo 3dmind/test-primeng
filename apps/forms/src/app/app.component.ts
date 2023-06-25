2d1cb00fadadb93703023ce29409acfaf81040b6
Christian Wlatschiha <c.wlatschiha@gmail.com>
2023-06-25 18:02:47 +0200

    feat: test inputs with floating labels

--- apps/forms/src/app/app.component.ts
+++ apps/forms/src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'test-primeng-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'forms';
-}
+export class AppComponent {}
--- apps/forms/src/app/app.module.ts
+++ apps/forms/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardModule } from 'primeng/card';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    CardModule,
+    InputTextModule,
+    InputNumberModule,
+    InputMaskModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
